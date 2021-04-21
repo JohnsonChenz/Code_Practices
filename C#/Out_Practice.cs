a80dd7e870905cc2270a19c7e266d7d75401684f
fouury <78896227+fouury@users.noreply.github.com>
2021-04-21 17:32:34 +0800

    Add files via upload

---
+++ C#/Out_Practice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Practice
+{
+
+    class Out_Practice
+    {
+        class Str
+        {
+            public string str = "";
+        }
+
+        /// <summary>
+        /// Out方法
+        /// </summary>
+        /// <param name="a"></param>
+        static void Out_Test_1(string x ,out Str obj)
+        {
+            obj = new Str();
+            obj.str = x + "OUT";
+        }
+
+        static void Out_Test_2(int x,out int Num)
+        {
+            Num = x * 2;
+        }
+        static void Main()
+        {
+            string test_str = Console.ReadLine();
+            Str a;
+            Out_Test_1(test_str, out a);
+            Console.WriteLine(a.str);
+
+            int num;
+            int test_num = Int32.Parse(Console.ReadLine());
+            Out_Test_2(test_num, out num);
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Ref_Practice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Practice
+{
+    class Ref_Practice
+    {
+        static void Main(string[] args)
+        {
+            int num = Int32.Parse(Console.ReadLine());
+            Ref_Test(ref num);
+            Console.WriteLine(num);
+            Console.ReadKey();
+        }
+
+        static void Ref_Test(ref int a)
+        {
+            a += 10;
+        }
+    }
+}
